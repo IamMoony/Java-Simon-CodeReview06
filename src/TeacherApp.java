6f44ac80c660c5741dafe667acdee7b40e49702d
Blaha Simon <sib@blaha.co.at>
2019-06-14 18:04:42 +0200

    reupload

--- src/TeacherApp.java
+++ src/TeacherApp.java
@@ -1,5 +1,8 @@
 import javafx.application.Application;
 import javafx.beans.Observable;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
@@ -11,14 +14,44 @@ import javafx.scene.paint.Color;
 import javafx.scene.text.TextAlignment;
 import javafx.stage.Stage;
 
-public class Main extends Application {
+import java.util.List;
+
+public class TeacherApp extends Application {
 
     private ListView<Teacher> listView;
     private ObservableList<Teacher> data;
     private TeacherDataAccess dbaccess;
+    private Label lblIdData;
+    private Label lblNameData;
+    private Label lblSurnameData;
+    private Label lblEmailData;
 
     public static void main(String[] args) {
         Application.launch(args);
+    }
+
+    @Override
+    public void init() {
+
+        try {
+            dbaccess = new TeacherDataAccess();
+        }
+        catch (Exception e) {
+
+            displayException(e);
+        }
+    }
+
+    @Override
+    public void stop() {
+
+        try {
+            dbaccess.closeDb();
+        }
+        catch (Exception e) {
+
+            displayException(e);
+        }
     }
 
     @Override
@@ -26,9 +59,17 @@ public class Main extends Application {
 
         BorderPane root = new BorderPane();
         Label lblTop = new Label("School Statistics of Teachers (1)");
+        Label lblId = new Label("ID");
+        Label lblName = new Label("Name");
+        Label lblSurname = new Label("Surname");
+        Label lblEmail = new Label("Email");
+        HBox idData = new HBox(lblId, lblIdData);
+        HBox nameData = new HBox(lblName, lblNameData);
+        HBox surnameData = new HBox(lblSurname, lblSurnameData);
+        HBox emailData = new HBox(lblEmail, lblEmailData);
         HBox hBoxLblTop = new HBox(lblTop);
+        VBox vBoxDataCenter = new VBox(idData, nameData, surnameData, emailData);
         // hBoxLblTop.setBackground(new Background(new BackgroundFill(Color.rgb(40, 40, 40), CornerRadii.EMPTY, Insets.EMPTY)));
-        HBox hBoxCenter = new HBox();
 
         root.setPadding(new Insets(15, 20, 10, 10));
 
@@ -36,7 +77,12 @@ public class Main extends Application {
 
         listView = new ListView<>();
 
-        listView.getSelectionModel().selectedIndexProperty().addListener();
+        listView.getSelectionModel().selectedIndexProperty().addListener(
+                new ListSelectChangeListener());
+        data = getDbData();
+        listView.setItems(data);
+        root.setLeft(listView);
+        root.setCenter(vBoxDataCenter);
 
         // TOP
         String cssLayoutHboxTop = "-fx-border-color: purple;\n" +
@@ -68,5 +114,49 @@ public class Main extends Application {
         primaryStage.setScene(scene);
         primaryStage.show();
 
+    }
+
+    private class ListSelectChangeListener implements ChangeListener<Number> {
+
+        @Override
+        public void changed(ObservableValue<? extends Number> ov,
+                            Number old_val, Number new_val) {
+
+            if ((new_val.intValue() < 0) || (new_val.intValue() >= data.size())) {
+
+                return; // invalid data
+            }
+
+            // set name and desc fields for the selected teacher
+            Teacher teacher = data.get(new_val.intValue());
+            //lblid.setText(teacher.getTeacherId());
+            lblNameData.setText(teacher.getTeacherName());
+            lblSurnameData.setText(teacher.getTeacherSurname());
+            lblEmailData.setText(teacher.getTeacherEmail());
+            // actionstatus.setText(todo.getName() + " - selected");
+        }
+    }
+
+    private ObservableList<Teacher> getDbData() {
+
+        List<Teacher> list = null;
+
+        try {
+            list = dbaccess.getAllRows();
+        }
+        catch (Exception e) {
+
+            displayException(e);
+        }
+
+        ObservableList<Teacher> dbData = FXCollections.observableList(list);
+        return dbData;
+    }
+
+    private void displayException(Exception e) {
+
+        System.out.println("###### Exception ######");
+        e.printStackTrace();
+        System.exit(0);
     }
 }
--- src/TeacherDataAccess.java
+++ src/TeacherDataAccess.java
@@ -1,4 +1,84 @@
-package PACKAGE_NAME;
+import javafx.collections.ObservableList;
 
+import java.sql.SQLException;
+import java.sql.DriverManager;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.List;
+import java.util.ArrayList;
 public class TeacherDataAccess {
+
+    private Connection conn;
+    private static final String teacherTable = "teacher";
+
+    public TeacherDataAccess()
+            throws SQLException, ClassNotFoundException {
+
+        // Class.forName("org.hsqldb.jdbc.JDBCDriver" );
+
+        //STEP 2: Check if JDBC driver is available
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        //STEP 3: Open a connection
+        System.out.println("Connecting to database...");
+        conn = DriverManager.getConnection(
+                "jdbc:mysql://localhost:3306/java_codereview06" +
+                        "?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC",
+                "root",
+                "");
+
+        // we will use this connection to write to a file
+        conn.setAutoCommit(true);
+        conn.setReadOnly(false);
+    }
+
+    public void closeDb() throws SQLException {
+        conn.close();
+    }
+
+    /**
+     * Get all db records
+     * @return
+     * @throws SQLException
+     */
+    public List<Teacher> getAllRows()  throws SQLException {
+
+        String sql = "SELECT * FROM " + teacherTable + " ORDER BY name";
+        PreparedStatement pstmnt = conn.prepareStatement(sql);
+        ResultSet rs = pstmnt.executeQuery();
+        List<Teacher> list = new ArrayList<>();
+
+        while (rs.next()) {
+            int i = rs.getInt("teacherId");
+            String name = rs.getString("teacherName");
+            String surname = rs.getString("teacherSurname");
+            String email = rs.getString("teacherEmail");
+            list.add(new Teacher(i, name, surname, email));
+        }
+
+        pstmnt.close(); // also closes related result set
+        return list;
+    }
+
+/*
+    public boolean nameExists(Teacher teacher) throws SQLException {
+
+        String sql = "SELECT COUNT(id) FROM " + teacherTable + " WHERE name = ? AND id <> ?";
+        PreparedStatement pstmnt = conn.prepareStatement(sql);
+        pstmnt.setString(1, teacher.getTeacherName());
+        pstmnt.setInt(2, teacher.getTeacherId());
+        ResultSet rs = pstmnt.executeQuery();
+        rs.next();
+        int count = rs.getInt(1);
+        pstmnt.close();
+
+        if (count > 0) {
+
+            return true;
+        }
+
+        return false;
+    }
+ */
 }
+
